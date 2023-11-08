2c25e21770b5dfdab1c4228238439c3d9655fb9b
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2023-11-07 22:44:27 -0300

    Improve navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,40 @@ export default function Navbar() {
     // eslint-disable-next-line no-unused-vars
     const [navbarOpen, setNavbarOpen] = useState(true);
 
-    // TODO: Add functionality to the menu button
+    const links = [
+        {
+            href: '/',
+            label: 'Home',
+        },
+        {
+            href: '/',
+            label: 'Catalogue',
+        },
+        {
+            href: '/',
+            label: 'Deluxe Line',
+        },
+        {
+            href: '/',
+            label: 'About',
+        },
+    ];
+
+    const desktopLinks = links.map((link) => (
+        <li key={link.label} className='inline-block hover:text-primary-600'>
+            <a href={link.href} className="p-4 font-thin transition duration-300  lg:px-8">
+                {link.label}
+            </a>
+        </li>
+    ));
+
+    const mobileLinks = links.map((link) => (
+        <li key={link.label} className='inline-block w-full border-b border-slate-600 p-4 text-center hover:text-primary-600'>
+            <a href={link.href} className="font-thin lg:px-8">
+                {link.label}
+            </a>
+        </li>
+    ));
 
     return (
         <nav className="bg-white text-slate-700 shadow md:backdrop-blur" role="navigation">
@@ -26,20 +59,9 @@ export default function Navbar() {
                 <a href="/" className="block p-4 font-display text-2xl tracking-wider transition duration-300 hover:text-primary-600">
                     Laretto
                 </a>
-                <div className="hidden uppercase md:block md:tracking-wide lg:tracking-widest">
-                    <a href="/" className="p-4 font-thin transition duration-300 hover:text-primary-600 lg:px-8">
-                        Home
-                    </a>
-                    <a href="/" className="p-4 font-thin transition duration-300 hover:text-primary-600 lg:px-8">
-                        Catalogue
-                    </a>
-                    <a href="/" className="p-4 font-thin transition duration-300 hover:text-primary-600 lg:px-8">
-                        Deluxe Line
-                    </a>
-                    <a href="/" className="p-4 font-thin transition duration-300 hover:text-primary-600 lg:px-8">
-                        About
-                    </a>
-                </div>
+                <ul className="hidden uppercase md:block md:tracking-wide lg:tracking-widest">
+                    { desktopLinks }
+                </ul>
                 <div className="flex">
                     { /* Hardcoded badge count for now */ }
                     <CartWidget badgeCount={ 42 } />
@@ -48,22 +70,10 @@ export default function Navbar() {
             </div>
 
             <div className="bg-slate-700 uppercase tracking-widest text-slate-100 md:hidden">
-                <div className="container mx-auto flex flex-col">
-                    <a href="/" className="p-4 font-thin hover:text-primary-600 lg:px-8">
-                        Home
-                    </a>
-                    <a href="/" className="p-4 font-thin hover:text-primary-600 lg:px-8">
-                        Catalogue
-                    </a>
-                    <a href="/" className="p-4 font-thin hover:text-primary-600 lg:px-8">
-                        Deluxe Line
-                    </a>
-                    <a href="/" className="p-4 font-thin hover:text-primary-600 lg:px-8">
-                        About
-                    </a>
-                </div>
+                <ul className="container mx-auto flex flex-col">
+                    { mobileLinks }
+                </ul>
             </div>
-
         </nav>
     );
 }
