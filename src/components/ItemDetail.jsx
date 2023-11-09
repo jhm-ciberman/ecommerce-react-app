a476f7af478ca0598604dee2fb8eed68722455f7
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2023-11-09 20:02:58 -0300

    Add ItemDetail and ItemDetailContainer

---
+++ src/components/ItemDetail.jsx
@@ -0,0 +1,7 @@
+export default function ItemDetail() {
+    return (
+        <div>
+            <h1>ItemDetail</h1>
+        </div>
+    );
+}
---
+++ src/components/ItemDetailContainer.jsx
@@ -0,0 +1,7 @@
+export default function ItemDetailContainer() {
+    return (
+        <div>
+            <h1>ItemDetailContainer</h1>
+        </div>
+    );
+}
