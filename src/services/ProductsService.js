ccbb4920882b9375d0ef1a0ebb3866a95d689170
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2023-12-06 01:51:11 -0300

    Swap FakeStoreAPI by Firestore db

--- src/services/ProductsService.js
+++ src/services/ProductsService.js
@@ -1,3 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    query,
+    where,
+    limit,
+    collection,
+    getDocs,
+    documentId,
+} from 'firebase/firestore';
+
 /**
  * @typedef {Object} Product
  * @property {number} id
@@ -28,6 +41,15 @@ export class FetchError extends Error {
     }
 }
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAaL5AJYvubd_F0JflaR_aWw-x9bYESgXs",
+    authDomain: "laretto-react-app.firebaseapp.com",
+    projectId: "laretto-react-app",
+    storageBucket: "laretto-react-app.appspot.com",
+    messagingSenderId: "831803443789",
+    appId: "1:831803443789:web:10e48ebfb6340fa6326a04"
+};
+
 export default class ProductsService {
 
     /** @private */
@@ -38,11 +60,20 @@ export default class ProductsService {
         return ProductsService._instance || (ProductsService._instance = new ProductsService());
     }
 
-    _endpoint = 'https://fakestoreapi.com';
+    _app = null;
 
-    _simulateDelay = true; // We could use the throttle from Chrome DevTools, but this is more fun.
+    _firestore = null;
 
-    _simulateErrorChance = 0.1; // 10% chance of failure. This is only for demonstration purposes.
+    constructor() {
+        if (ProductsService._instance) {
+            throw new Error('ProductsService is a singleton');
+        }
+
+        ProductsService._instance = this;
+
+        this._app = initializeApp(firebaseConfig);
+        this._firestore = getFirestore(this._app);
+    }
 
     _categorySlugToName = { // key: slug, value: name as required by the API
         "electronics": "electronics",
@@ -51,67 +82,12 @@ export default class ProductsService {
         "womens-clothing": "women's clothing",
     };
 
-    _fakeDelay() {
-        if (!this._simulateDelay) {
-            return Promise.resolve();
-        }
-
-        return new Promise(resolve => setTimeout(resolve, Math.random() * 1000 + 600));
-    }
-
-    _fakeError() {
-        if (Math.random() < this._simulateErrorChance) {
-            throw new Error('Simulated network error');
-        }
-    }
-
-    _ensureContentLength(response) {
-        // FakeStoreAPI returns 200 with empty body instead of 404 for missing resource. Let's fix it client side.
-        if (response.headers.get('Content-Length') === '0') {
-            return new window.Response(response.body, {
-                status: 404,
-                headers: response.headers
-            });
-        }
-        return response;
-    }
-
-    _ensureOk(response) {
-        if (!response.ok) {
-            throw new FetchError(response.statusText, response);
-        }
-        return response;
-    }
-
-    _handleError(error) {
-        console.error('API error:', error);
-        throw error;
-    }
-
-    /**
-     * Fetch data from the API.
-     *
-     * @param {string} url
-     * @returns {Promise<any>}
-     */
-    _get(url) {
-        // In a real world scenario, we would use Axios, because it has better built in error handling than native fetch
-        // Just look at this ugly mess:
-        return this._fakeDelay()
-            .then(() => this._fakeError())
-            .then(() => fetch(`${this._endpoint}${url}`))
-            .then(response => this._ensureContentLength(response))
-            .then(response => this._ensureOk(response))
-            .then(response => response.json())
-            .catch(error => this._handleError(error));
-    }
-
     /**
      * Ensure that the array has at least `minimum` elements, repeating the array if necessary.
      *
-     * @param {Product[]} array
+     * @param {object[]} array
      * @param {number} minimum
-     * @returns {Product[]}
+     * @returns {object[]}
      */
     _take(array, minimum) {
         const result = [];
@@ -122,13 +98,35 @@ export default class ProductsService {
     }
 
     /**
+     * Ensure that the query snapshot has at least `minimum` elements, repeating the array if necessary.
+     *
+     * @param {import('firebase/firestore').QuerySnapshot} querySnapshot
+     * @param {number} minimum
+     * @returns {object[]}
+     */
+    _takeDocs(querySnapshot, minimum) {
+        return this._take(querySnapshot.docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id,
+        })), minimum);
+    }
+
+    /**
      * Get all products from the API.
      *
      * @returns {Promise<Product[]>}
      */
-    getIndexProducts() {
-        return this._get('/products')
-            .then((products) => this._take(products, 24));
+    async getIndexProducts() {
+        const max = 24;
+
+        const q = query(
+            collection(this._firestore, 'products'),
+            limit(max),
+        );
+
+        const querySnapshot = await getDocs(q);
+
+        return this._takeDocs(querySnapshot, max);
     }
 
     /**
@@ -137,42 +135,60 @@ export default class ProductsService {
      * @param {number} id The product ID
      * @returns {Promise<Product>}
      */
-    getProduct(id) {
-        return this._get(`/products/${id}`)
-            .then(async (product) => {
-                return {
-                    ...product,
-                    min: 1,
-                    max: 10,
-                    related: await this._getRelatedProducts(product)
-                };
-            }
-        );
+    async getProduct(id) {
+        const docRef = doc(this._firestore, 'products', id.toString());
+        const docSnapshot = await getDoc(docRef);
+
+        const product = ({
+            ...docSnapshot.data(),
+            id: docSnapshot.id,
+            min: 1,
+            max: 10,
+        });
+
+        return {
+            ...product,
+            related: await this._getRelatedProducts(product)
+        };
     }
 
-    _getRelatedProducts(product) {
-        return this._get(`/products/category/${product.category}`)
-            .then((products) => {
-                // Filter out the current product from the related products
-                return products.filter((p) => p.id !== product.id);
-            })
-            .then((products) => this._take(products, 6));
+    async _getRelatedProducts(product) {
+        const max = 6;
+
+        const q = query(
+            collection(this._firestore, 'products'),
+            where('category', '==', product.category),
+            where(documentId(), '!=', product.id),
+            limit(max)
+        );
+
+        const querySnapshot = await getDocs(q);
+
+        return this._takeDocs(querySnapshot, max);
     }
 
     /**
      * Gets the given category from the API with all the items in it.
      *
      * @param {string} slug The category slug
-     * @returns {Promise<{ name: string, Product[]}>}
+     * @returns {Promise<{ name: string, items: Product[]}>}
      */
-    getCategory(slug) {
+    async getCategory(slug) {
         const name = this._categorySlugToName[slug];
-        return this._get(`/products/category/${name}`)
-            .then((items) => ({
-                // In a real world scenario, we would fetch in the same request the category name, banner image, navigation breadcrumbs
-                // and pagination metadata(nextPage, previousPage, etc.) and return them here in this object.
-                name: name,
-                items: this._take(items, 24),
-            }));
+
+        const max = 24;
+
+        const querySnapshot = await getDocs(
+            query(
+                collection(this._firestore, 'products'),
+                where('category', '==', name),
+                limit(max) // In a real world scenario, we would use pagination
+            )
+        );
+
+        return {
+            name: name,
+            items: this._takeDocs(querySnapshot, max),
+        };
     }
 }
